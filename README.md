c5f6355483511c0ccf1d975946022cabda7c40bc
Thales Pereira <thales.claro@gmail.com>
2018-12-17 10:29:14 +0000

    base import

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# pi_kube
-Kubernetes setup for raspberry pi 3 B+
+# pi-kube
+
+Deployment and configuration of Kubernetes on raspberry pi 3 b+
+
+
+# Usage
+
+#### Writing raspian image on micro sd
+First you must use `raspian_install.sh` for writing [Raspbian Stretch Lite](https://www.raspberrypi.org/downloads/raspbian/) on your sd card, this script will also suggest you to enable ssh connections which consists on creating a ssh file where `boot` mount is placed. For those who are not aware, default credentials are `pi:raspberry`. Make sure to disable password at the sshd config file and copy your public key using `ssh-copy-id pi@<ip>` 
+
+
+#### Deployment of network definitions, docker and kubernetes
+
+Simply execute `raspbian_install.sh` , and the following actions will be take care of:
+- Network setup by defining a static ip
+- Update Kernel kernel arguments necessary for kubernetes on `/boot/cmdline.txt` file
+- Swap disable
+- Install Docker/Kubernetes
+- Setup Weave Network
+
---
+++ sd_write.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e 
+
+source config.ini
+
+# Download Raspbian Image
+OUTFILE='raspbian_lite.zip'
+wget https://downloads.raspberrypi.org/raspbian_lite_latest -O "$OUTFILE"
+unzip "$OUTFILE"
+
+# Write Image to sd 
+IMG_FILE=$(ls *.img)
+read -p "Plug your microsd card and press enter to continue"
+df -h | grep "/dev"
+echo "Confirm and type sd card location ( eg: /dev/xpto ): "
+read SD_LOCATION
+echo "Burning image..."
+sudo dd bs=4M if="$IMG_FILE" of="$SD_LOCATION" conv=fsync
+
+
+# Touch ssh file for enabling ssh server
+df 
+echo "Confirm and type boot mount point ( eg: /run/media/user/boot ) : "
+read BOOT_PATH
+sudo touch "$BOOT_PATH/ssh"
+# sync and umount
+sync
+umount $ROOT_PATH
+
+echo "unplug boot your new pi and wait for ssh authentication"
+while true; do
+    ping -c 1 raspberrypi.local && break
+done
+
+echo "adding public key with default password"
+sshpass -p raspberry ssh-copy-id pi@raspberrypi.local
+echo "Copying files to our new friend and deploy kubernetes, sit back and relax"
+scp config.ini run.sh pi@raspberrypi.local:/home/pi
+ssh pi@raspberrypi.local sh -c './run.sh'
