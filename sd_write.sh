3d0ce961c58acf0c806fb762c7ebd618b3d0e321
Thales Pereira <thales.claro@gmail.com>
2018-12-17 15:15:02 +0000

    comments updated

--- sd_write.sh
+++ sd_write.sh
@@ -3,13 +3,15 @@
 set -e 
 
 source config.ini
-
+#
 # Download Raspbian Image
+#
 OUTFILE='raspbian_lite.zip'
 wget https://downloads.raspberrypi.org/raspbian_lite_latest -O "$OUTFILE"
 unzip "$OUTFILE"
-
+#
 # Write Image to sd 
+#
 IMG_FILE=$(ls *.img)
 read -p "Plug your microsd card and press enter to continue"
 df -h | grep "/dev"
@@ -17,24 +19,30 @@ echo "Confirm and type sd card location ( eg: /dev/xpto ): "
 read SD_LOCATION
 echo "Burning image..."
 sudo dd bs=4M if="$IMG_FILE" of="$SD_LOCATION" conv=fsync
-
-
+#
 # Touch ssh file for enabling ssh server
+#
 df 
 echo "Confirm and type boot mount point ( eg: /run/media/user/boot ) : "
 read BOOT_PATH
 sudo touch "$BOOT_PATH/ssh"
+#
 # sync and umount
+#
 sync
 umount $ROOT_PATH
-
+#
+# Wait for your new friend to be available and setup key auth.
+#
 echo "unplug boot your new pi and wait for ssh authentication"
 while true; do
     ping -c 1 raspberrypi.local && break
 done
-
-echo "adding public key with default password"
+echo "adding public key with default password, PLEASE CHANGE IT!!!"
 sshpass -p raspberry ssh-copy-id pi@raspberrypi.local
+#
+# Copy config and setup script to your new node and install docker/kubernetes
+#
 echo "Copying files to our new friend and deploy kubernetes, sit back and relax"
 scp config.ini run.sh pi@raspberrypi.local:/home/pi
 ssh pi@raspberrypi.local sh -c './run.sh'
--- setup.sh
+++ setup.sh
@@ -39,17 +39,16 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -qqy 
 sudo apt-get install -qqy kubeadm
-
+#
+# Init and deploy weave network for master
+#
 if [ "$NODE_TYPE" == 'master' ]; then
     sudo kubeadm init
     mkdir -p $HOME/.kube
     sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
     #Setup weave network
     kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
-    
 fi
 echo "Done"
 sudo shutdown -r now
