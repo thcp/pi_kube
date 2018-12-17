56d22e5a14f9cda16795b77cffe30273aa3a96e5
Thales Pereira <thales.claro@gmail.com>
2018-12-17 12:06:15 +0000

    build test script for travis added

---
+++ build_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e 
+source config.ini
+
+#
+# Disable swap
+#
+sudo dphys-swapfile swapoff
+sudo dphys-swapfile uninstall
+sudo update-rc.d dphys-swapfile remove
+#
+# Install Docker
+#
+curl -sSL get.docker.com | sh && sudo usermod pi -aG docker
+#
+# Install Kubernetes
+#
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -qqy 
+sudo apt-get install -qqy kubeadm
+
+sudo kubeadm init
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#Setup weave network
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
